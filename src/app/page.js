146d9685061d9f2c41045200c9ff13529d7be166
Maria Clara Carvalho <cabohyun@hotmail.com>
2025-12-25 01:27:44 -0300

    Assunto: estrutura oficial do projeto

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Meu Projeto Ca-bo-hyun</h1>
+      <p>Projeto sendo reconstruído! 25/12/2025</p>
+    </main>
+  );
+}
